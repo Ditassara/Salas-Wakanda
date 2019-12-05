3de048c3fd4721a39743ef229c9d586a209cb72d
Ditassara <56659426+Ditassara@users.noreply.github.com>
2019-12-05 20:42:44 -0300

    Update urls.py

--- wakanda/urls.py
+++ wakanda/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-	path('',views.index,name='index'),
+	path('index',views.index,name='index'),
 	path('salas/',views.salas,name='salas'),
 	# path('arriendo/<int:pk>', views.ArriendoDetailView.as_view(), name='arriendo-detail'),
 ]
