78523c401e47fa7052d31976d3084b81705f1b79
Ditassara <56659426+Ditassara@users.noreply.github.com>
2019-12-05 22:30:34 -0300

    Update urls.py

--- wakanda/urls.py
+++ wakanda/urls.py
@@ -7,9 +7,9 @@ urlpatterns=[
 	# path('arriendo/<int:pk>', views.ArriendoDetailView.as_view(), name='arriendo-detail'),
 ]
 
-urlpatterns += [
-	path('accounts/',include('django.contrib.auth.urls')),
+#urlpatterns += [
+	
 # 	path('arriendo/create/', views.ArriendoCreate.as_view(), name='arriendo_create'),
 # 	path('arriendo/<int:pk>/update/', views.ArriendoUpdate.as_view(), name='arriendo_update'),
 # 	path('arriendo/<int:pk>/delete/', views.ArriendoDelete.as_view(), name='arriendo_delete'),
-]
+#]
