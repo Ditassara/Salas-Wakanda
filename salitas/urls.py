ceb2937a0447d325f3707b565683cf989f9e9f7b
Ditassara <56659426+Ditassara@users.noreply.github.com>
2019-12-05 22:31:01 -0300

    Update urls.py

--- salitas/urls.py
+++ salitas/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
    	path('admin/', admin.site.urls),
 	path('', include('wakanda.urls')),
 	#path(r'^wakanda/', include('wakanda.urls')),
+	path('accounts/',include('django.contrib.auth.urls')),
 ]
 
 urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
